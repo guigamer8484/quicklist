ce509ac6bfa95ae4a23c34e560a968a8139017f2
Guilherme Queiroz <guilhermequeiroz8484@gmail.com>
2025-05-30 23:29:36 -0300

    verificação do preenchimento da input  #item

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let items = []
 function addItem(){
     const itemName = document.querySelector("#item").value
 
+    if (itemName === ""){
+        alert("Digite um item válido")
+        return
+    }
+
     const item = {
         nome: itemName,
         checked: false
